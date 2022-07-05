889c4ea4b8896c2afe913190d60979aa804f97ed
rana haseeb <alix@yahoo.com>
2022-07-05 15:00:59 +0500

    multiple_ways_of_params_methods

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,18 +1,16 @@
 class PeopleController < ApplicationController
-  def create
-    Person.create(params[:person])
-  end
 
-  def update
-    person = current_account.people.find(params[:id])
-    person.update!(person_params)
-    redirect_to person
-  end
-   private
-  def person_params
-      params.require(:person).permit(:name, :age)
-      params.permit(:id)
-      params.permit(id: [])
-      params.require(:log_entry).permit!
-    end
+      params.permit(:name, { emails: [] },
+              friends: [ :name,
+                         { family: [ :name ], hobbies: [] }])
+
+    params.fetch(:blog, {}).permit(:title, :author)
+
+    params.require(:author).permit(:name, books_attributes: [:title, :id, :_destroy])
+   params.require(:book).permit(:title, chapters_attributes: [:title])
+
+   def product_params
+  params.require(:product).permit(:name, data: params[:product][:data].try(:keys))
+end
+
 end
