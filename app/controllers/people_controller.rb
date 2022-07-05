026e87cd393f5a6e9b97d18d539ef17ecbc2c3f9
rana haseeb <alix@yahoo.com>
2022-07-05 14:44:47 +0500

    strong_params

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,16 @@
+class PeopleController < ApplicationController
+  def create
+    Person.create(params[:person])
+  end
+
+  def update
+    person = current_account.people.find(params[:id])
+    person.update!(person_params)
+    redirect_to person
+  end
+   private
+  def person_params
+      params.require(:person).permit(:name, :age)
+
+    end
+end
