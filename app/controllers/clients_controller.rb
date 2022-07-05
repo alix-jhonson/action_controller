c88f1c3444958c48310fc1459a99ea0501f3d713
rana haseeb <alix@yahoo.com>
2022-07-05 14:14:59 +0500

    action_controller_params

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,4 +2,20 @@ class ClientsController < ApplicationController
   def new
    @client = Client.new
   end
+  def index
+    if params[:status] == "activated"
+      @clients = Client.activated
+    else
+      @clients = Client.inactivated
+    end
+  end
+
+  def create
+    @client = Client.new(params[:client])
+    if @client.save
+      redirect_to @client
+    else
+      render "new"
+   end
+  end
 end
