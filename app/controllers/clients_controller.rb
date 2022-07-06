61e0a0df6c8ad8e45f9571751a22824b4f9e9e70
rana haseeb <alix@yahoo.com>
2022-07-06 16:09:56 +0500

    sending_file_method code

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,22 +1,8 @@
 class ClientsController < ApplicationController
-  require "prawn"
-class ClientsController < ApplicationController
-  # Generates a PDF document with information on the client and
-  # returns it. The user will get the PDF as a file download.
   def download_pdf
     client = Client.find(params[:id])
-    send_data generate_pdf(client),
+    send_file("#{Rails.root}/files/clients/#{client.id}.pdf",
               filename: "#{client.name}.pdf",
-              type: "application/pdf"
+              type: "application/pdf")
   end
-
-  private
-
-    def generate_pdf(client)
-      Prawn::Document.new do
-        text client.name, align: :center
-        text "Address: #{client.address}"
-        text "Email: #{client.email}"
-      end.render
-    end
 end
