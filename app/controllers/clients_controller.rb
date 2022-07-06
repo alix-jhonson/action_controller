5c30b047dfd34841edaf25b65c23aee7c8cd798b
rana haseeb <alix@yahoo.com>
2022-07-06 12:02:33 +0500

    flash.now_method_in_action_controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,21 +1,11 @@
 class ClientsController < ApplicationController
-  def new
-   @client = Client.new
-  end
-  def index
-    if params[:status] == "activated"
-      @clients = Client.activated
-    else
-      @clients = Client.inactivated
-    end
-  end
-
   def create
     @client = Client.new(params[:client])
     if @client.save
-      redirect_to @client
+      # ...
     else
-      render "new"
-   end
+      flash.now[:error] = "Could not save client"
+      render action: "new"
+    end
   end
 end
