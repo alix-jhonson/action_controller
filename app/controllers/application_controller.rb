3580e8587cdbc2fea096adaf840fb64fb950d379
rana haseeb <alix@yahoo.com>
2022-07-06 13:27:05 +0500

    filter_method_in_action_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-private
-def current_user
-    @_current_user ||= session[:current_user_id] &&
-      User.find_by(id: session[:current_user_id])
-  end
+before_action :require_login
 
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_login_url # halts request cycle
+    end
+  end
+end
 
 
   protect_from_forgery with: :exception
--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,11 +1,5 @@
 class LoginsController < ApplicationController
-   def destroy
-    session[:current_user_id] = nil
-    redirect_to root_url, notice: "You have successfully logged out."
-    redirect_to root_url, alert: "You're stuck here!"
-    redirect_to root_url, flash: { referral_code: 1234 }
-
-  end
+  skip_before_action :require_login, only: [:new, :create]
 end
 
 
