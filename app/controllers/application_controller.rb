84e9f4293ae7d91cadc618ffb5f693ad0e09d2b0
rana haseeb <alix@yahoo.com>
2022-07-06 13:39:18 +0500

    using filter through class in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-before_action do |controller|
+  before_action LoginFilter
+end
+
+class LoginFilter
+  def self.before(controller)
     unless controller.send(:logged_in?)
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to new_login_url
+      controller.flash[:error] = "You must be logged in to access this section"
+      controller.redirect_to controller.new_login_url
     end
   end
 end
-
-
-
 
 
   protect_from_forgery with: :exception
