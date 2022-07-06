3d299462da21e4a5d6c85d3520cfdfc3dfab8022
rana haseeb <alix@yahoo.com>
2022-07-06 11:06:01 +0500

    the flesh method in action_controller

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,6 +1,7 @@
 class LoginsController < ApplicationController
-  def destroy
-    @_current_user = session[:current_user_id] = nil
+   def destroy
+    session[:current_user_id] = nil
+    flash[:notice] = "You have successfully logged out."
     redirect_to root_url
   end
 end
