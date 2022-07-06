9a7d600340f8453991c8d49b66c0f92df502eead
rana haseeb <alix@yahoo.com>
2022-07-06 16:11:53 +0500

    restful downloads method

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,8 +1,10 @@
 class ClientsController < ApplicationController
-  def download_pdf
-    client = Client.find(params[:id])
-    send_file("#{Rails.root}/files/clients/#{client.id}.pdf",
-              filename: "#{client.name}.pdf",
-              type: "application/pdf")
+  def show
+    @client = Client.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.pdf { render pdf: generate_pdf(@client) }
+    end
   end
 end
