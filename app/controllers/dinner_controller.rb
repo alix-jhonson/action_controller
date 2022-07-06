7cc1bd0bf7a401c46125cbf2195883b61ef205b9
rana haseeb <alix@yahoo.com>
2022-07-06 17:10:43 +0500

    force_https_protocol_method

---
+++ app/controllers/dinner_controller.rb
@@ -0,0 +1,9 @@
+class DinnerController
+
+force_ssl
+# with :only and :except
+force_ssl only: :cheeseburger
+
+force_ssl except: :cheeseburger
+
+end
---
+++ test/controllers/dinner_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DinnerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
