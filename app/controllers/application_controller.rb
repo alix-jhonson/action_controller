ee8237d635fa9729d9ac4b400c305bf25ca36d6f
rana haseeb <alix@yahoo.com>
2022-07-06 17:06:06 +0500

    rescue_from_functionality_through_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,40 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_action LoginFilter
-end
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-class LoginFilter
-  def self.before(controller)
-    unless controller.send(:logged_in?)
-      controller.flash[:error] = "You must be logged in to access this section"
-      controller.redirect_to controller.new_login_url
+  private
+
+    def record_not_found
+      render plain: "404 Not Found", status: 404
     end
-  end
+
+rescue_from User::NotAuthorized, with: :user_not_authorized
+
+  private
+
+    def user_not_authorized
+      flash[:error] = "You don't have access to this section."
+      redirect_to :back
+    end
 end
 
+class ClientsController < ApplicationController
+  # Check that the user has the right authorization to access clients.
+  before_action :check_authorization
+
+  # Note how the actions don't have to worry about all the auth stuff.
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  private
+
+    # If the user is not authorized, just throw the exception.
+    def check_authorization
+      raise User::NotAuthorized unless current_user.admin?
+    end
+end
 
   protect_from_forgery with: :exception
 end
