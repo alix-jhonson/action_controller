4a4c75068984472100dbfe628a0ff7f18d29a8d7
rana haseeb <alix@yahoo.com>
2022-07-06 13:37:41 +0500

    other way of using filter method code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-before_action :require_login
-
-  private
-
-  def require_login
-    unless logged_in?
+before_action do |controller|
+    unless controller.send(:logged_in?)
       flash[:error] = "You must be logged in to access this section"
-      redirect_to new_login_url # halts request cycle
+      redirect_to new_login_url
     end
   end
 end
+
+
+
 
 
   protect_from_forgery with: :exception
