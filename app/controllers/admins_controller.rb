d2f62c0ab882e73f7533b61145584e69482fdf98
rana haseeb <alix@yahoo.com>
2022-07-06 15:53:49 +0500

    http_digest_authentication_method

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,3 +1,15 @@
 class AdminsController < ApplicationController
-  http_basic_authenticate_with name: "humbaba", password: "5baa61e4"
+
+  USERS = { "lifo" => "world" }
+
+  before_action :authenticate
+
+  private
+
+    def authenticate
+      authenticate_or_request_with_http_digest do |username|
+        USERS[username]
+      end
+    end
+end
 end
